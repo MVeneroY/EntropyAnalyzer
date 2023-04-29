954f5ceab496a45bfd6a1b3a203dad25e6e57bd4
Miguel Venero-Yupanqui <73001225+MVeneroY@users.noreply.github.com>
2023-04-28 21:55:19 -0400

    populate_csv subprocess call

--- scripts/populate_csv.py
+++ scripts/populate_csv.py
@@ -1,6 +1,7 @@
 #! /usr/local/bin/python3
 
 import sys
+import subprocess
 from typing import List
 
 total_items = []
@@ -14,11 +15,31 @@ def main(directories: List[int]):
 
 def getData(directory: str, packed: bool = False):
     # get file name
+    files = []
+
     # get file size
-    # get entropy
-    
+
+    # get entropy (TODO: get output from entropy calls)
+    executable_path = '../target/entropy'
+    try:
+        for file in files:
+            subprocess.run(
+                [executable_path, file],
+                timeout=5,
+                check=True
+            )
+    except FileNotFoundError as e:
+        print(f"Process failed because executable was not found. Make sure you provide a working file path\n{e}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Process failed, return code was not successful."
+              f"Returned {e.returncode}\n{e}")
+    except subprocess.TimeoutExpired as e:
+        print(f"Process timed out.\n{e}")
+
+
+
+
     # push {$name, $size, $entropy, $packed} to total_items 
-    pass 
 
 if __name__ == '__main__':
     print("hey", sys.argv)
